cc93ee5a8c6059ad30b42aa09d7e95b1e2342f27
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2019-11-29 17:49:25 -0500

    Add VC for products with a CollectionView and custom ColectionViewCell

---
+++ coder-shop/coder-shop/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-shop
+//
+//  Created by Kilyan SOCKALINGUM on 29/11/2019.
+//  Copyright © 2019 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice:UILabel!
+    
+}
