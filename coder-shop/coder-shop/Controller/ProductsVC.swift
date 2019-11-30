348581ce75650e785edd185711cb0e31f8266527
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2019-11-29 19:25:33 -0500

    Send selected category to ProductVC and display products accordingly. App finished

---
+++ coder-shop/coder-shop/Controller/ProductsVC.swift
@@ -0,0 +1,42 @@
+//
+//  ProductsVC.swift
+//  coder-shop
+//
+//  Created by Kilyan SOCKALINGUM on 29/11/2019.
+//  Copyright © 2019 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
+    private(set) public var products = [Product]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func initProducts(category: Category) {
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateView(product: product)
+            return cell
+        }
+        
+        return ProductCell()
+    }
+    
+}
